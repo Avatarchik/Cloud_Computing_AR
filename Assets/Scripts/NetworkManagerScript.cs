9739ef475121cc6fb87a6fbaa6ff53bd5b5a825a
John Yuan <yjohnyuan@gmail.com>
2019-03-11 01:33:00 -0700

    So Far So Good

--- Assets/Scripts/NetworkManagerScript.cs
+++ Assets/Scripts/NetworkManagerScript.cs
@@ -1,129 +1,94 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NetworkManagerScript : MonoBehaviour {
+public class NetworkManagerScript : MonoBehaviour
+{
+
+    public Text TextInfos;
+    private Transform SpawnPoint1;
+    private Transform SpawnPoint2;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        PhotonNetwork.ConnectUsingSettings("v01");
+
+        SpawnPoint1 = GameObject.FindGameObjectWithTag("sp1").transform;
+        SpawnPoint2 = GameObject.FindGameObjectWithTag("sp2").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+ 
+
+        if (PhotonNetwork.connectionStateDetailed.ToString() != "Joined")
+        {
+            TextInfos.text = PhotonNetwork.connectionStateDetailed.ToString();
+        }
+        else
+        {
+            TextInfos.text = "Connected to " + PhotonNetwork.room.name + " Player(s) Online: " + PhotonNetwork.room.PlayerCount;
+        }
+
+    }
 
 
-	public Text TextInfos;
-	public Transform SpawnPoint1;
-	public Transform SpawnPoint2;
-	public GameObject[] players;
-	private bool gameStarted = false;
-	Text TxtHealth;
-	Text TxtHealthOther;
-	Text newGameText;
+    void OnConnectedToMaster()
+    {
+        Debug.Log("Connected with Master");
+        PhotonNetwork.JoinLobby();
+    }
 
+    void OnJoinedLobby()
+    {
+        RoomOptions MyRoomOptions = new RoomOptions();
+        MyRoomOptions.MaxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom("Room1", MyRoomOptions, TypedLobby.Default);
 
-	// Use this for initialization
-	void Start () {
+        Debug.Log("Connected with Lobby");
 
-		PhotonNetwork.ConnectUsingSettings ("v01");
-		newGameText = GameObject.Find ("newGameText").GetComponent<Text> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
-		if (PhotonNetwork.connectionStateDetailed.ToString () != "Joined") {
-			TextInfos.text = PhotonNetwork.connectionStateDetailed.ToString ();
-		} else {
-			TextInfos.text = "Connected to " + PhotonNetwork.room.name + " Player(s) Online: " + PhotonNetwork.room.PlayerCount;
-		}
+    void OnJoinedRoom()
+    {
 
+        if (PhotonNetwork.playerList.Length > 1)
+        {
+            StartCoroutine(SpawnMyPlayer());
+        }
+        else
+        {
+            StartCoroutine(SpawnMyPlayer2());
+        }
+    }
 
-		players = GameObject.FindGameObjectsWithTag ("Player");
+    IEnumerator SpawnMyPlayer()
+    {
+        yield return new WaitForSeconds(1);
 
-		if (players.Length == 2) 
-		{
-			gameStarted = true;
-		}
+        float xPositon = SpawnPoint1.position.x;
+        float zPositon = SpawnPoint1.position.z;
 
-		if (gameStarted) 
-		{
+        Vector3 position = new Vector3(xPositon,-1.65f, zPositon);
 
-			if (players.Length <= 1) 
-			{
-				for (int i = 0; i < players.Length; i++) 
-				{
-					Destroy (players [i]);
-					StartCoroutine (startNewRound ());
-					gameStarted = false;
+        GameObject MyPlayer = PhotonNetwork.Instantiate("Tank", position, Quaternion.identity, 0) as GameObject;
+    }
 
-					TxtHealth = GameObject.Find ("TxtHealth").GetComponent<Text> ();
-					TxtHealthOther = GameObject.Find ("TxtHealthOther").GetComponent<Text> ();
-					TxtHealth.text = "Player 1 100%";
-					TxtHealthOther.text = "Player 2 100%";
+    IEnumerator SpawnMyPlayer2()
+    {
+        yield return new WaitForSeconds(1);
 
-				}
-			}
-		}
-	}
+        float xPositon = SpawnPoint2.position.x;
+        float zPositon = SpawnPoint2.position.z;
+        Vector3 position = new Vector3(xPositon, -1.65f, zPositon);
 
+        //MyPlayer1 = PhotonNetwork.Instantiate("Tank", SpawnPoint2.position, Quaternion.identity, 0) as GameObject;
+        GameObject MyPlayer = PhotonNetwork.Instantiate("Tank", position, Quaternion.identity, 0) as GameObject;
+    }
 
-
-	IEnumerator startNewRound()
-	{
-		newGameText.text = "3";
-		yield return new WaitForSeconds (1f);
-		newGameText.text = "2";
-		yield return new WaitForSeconds (1f);
-		newGameText.text = "1";
-		yield return new WaitForSeconds (1f);
-		newGameText.text = "GO";
-		yield return new WaitForSeconds (1f);
-		newGameText.text = " ";
-
-		//StartCoroutine (SpawnMyPlayer ());
-
-		int randomX = Random.Range (-40, +40);
-
-		PhotonNetwork.Instantiate ("Tank", new Vector3 (randomX, 0, -50), Quaternion.identity, 0);
-	}
-	
-
-
-	void OnConnectedToMaster()
-	{
-		Debug.Log ("Connected with Master");
-		PhotonNetwork.JoinLobby ();
-	}
-
-	void OnJoinedLobby()
-	{
-		RoomOptions MyRoomOptions = new RoomOptions ();
-		MyRoomOptions.MaxPlayers = 2;
-		PhotonNetwork.JoinOrCreateRoom ("Room1", MyRoomOptions, TypedLobby.Default);
-
-		Debug.Log ("Connected with Lobby");
-
-	}
-
-	void OnJoinedRoom()
-	{
-
-		if (PhotonNetwork.playerList.Length > 1) {
-			StartCoroutine (SpawnMyPlayer ());
-		} 
-		else 
-		{
-			StartCoroutine (SpawnMyPlayer2 ());
-		}
-	}
-
-	IEnumerator SpawnMyPlayer()
-	{
-		yield return new WaitForSeconds (1f);
-		GameObject MyPlayer = PhotonNetwork.Instantiate ("Tank", SpawnPoint1.position, Quaternion.identity, 0) as GameObject;
-
-	}
-
-	IEnumerator SpawnMyPlayer2()
-	{
-		yield return new WaitForSeconds (1);
-		GameObject MyPlayer = PhotonNetwork.Instantiate ("Tank", SpawnPoint2.position, Quaternion.identity, 0) as GameObject;
-
-	}
-		
 }
---
+++ Assets/Scripts/placeMapScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARFoundation;
+
+
+public class placeMapScript : MonoBehaviour {
+
+	public Button StartGameBtn;
+	public Button PlaceGameObjectBtn;
+	public Button PlaceNewBtn;
+
+  	private ARPlaneManager ARPlaneManager;
+    private ARPointCloudManager ARPointCloudManager;
+    private ARPlane ARPlane;
+    private ARPlaneMeshVisualizer ARPlaneMeshVisualizer;
+    private ARPointCloud ARPointCloud;
+    private ARPointCloudParticleVisualizer ARPointCloudParticleVisualizer;
+
+
+	public void onClickPlaceGameObject ()
+	{
+
+		StartGameBtn.gameObject.SetActive (true);
+		PlaceNewBtn.gameObject.SetActive (true);
+		PlaceGameObjectBtn.gameObject.SetActive (false);
+
+
+		ARPlaneManager = GameObject.Find("AR Session Origin").GetComponent<ARPlaneManager>();
+        ARPointCloudManager = GameObject.Find("AR Session Origin").GetComponent<ARPointCloudManager>();
+        Destroy (ARPointCloudManager);
+        Destroy(ARPlaneManager);
+
+        ARPlane = GameObject.FindGameObjectWithTag("arPlaneTag").GetComponent<ARPlane>();
+        ARPlaneMeshVisualizer  = GameObject.FindGameObjectWithTag("arPlaneTag").GetComponent<ARPlaneMeshVisualizer>();
+        Destroy(ARPlane);
+        Destroy(ARPlaneMeshVisualizer);
+
+        ARPointCloud = GameObject.FindGameObjectWithTag("arPointTag").GetComponent<ARPointCloud>();
+        ARPointCloudParticleVisualizer = GameObject.FindGameObjectWithTag("arPointTag").GetComponent<ARPointCloudParticleVisualizer>();
+        Destroy(ARPointCloud);
+        Destroy(ARPlaneMeshVisualizer);
+        Destroy(GameObject.FindGameObjectWithTag("arPointTag"));
+	}
+
+	public void onClickPlaceNew ()
+	{
+		SceneManager.LoadScene ("MultiplayerTankGame");
+	}
+
+}
